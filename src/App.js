a30d59557d7b13b8d965a1dfbafc0438fb6c007f
Dem0ns <www@yangblog.cn>
2020-01-12 14:08:02 +0800

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,89 @@
 import React from 'react';
-import logo from './logo.svg';
+import CorsRemote from './CORS/remote';
 import './App.css';
+import {Icon, Menu, Layout} from "antd";
+const { SubMenu } = Menu;
+const {Footer, Sider, Content} = Layout;
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+
+    rootSubmenuKeys = ['sub1', 'sub2', 'sub4'];
+
+    state = {
+        openKeys: ['sub1'],
+    };
+
+    onOpenChange = openKeys => {
+        const latestOpenKey = openKeys.find(key => this.state.openKeys.indexOf(key) === -1);
+        if (this.rootSubmenuKeys.indexOf(latestOpenKey) === -1) {
+            this.setState({ openKeys });
+        } else {
+            this.setState({
+                openKeys: latestOpenKey ? [latestOpenKey] : [],
+            });
+        }
+    };
+
+    render() {
+        return (
+            <div>
+                <Layout>
+                    <Sider
+                        style={{
+                            overflow: 'auto',
+                            height: '100vh',
+                            position: 'fixed',
+                            left: 0,
+                        }}
+                    >
+                        <div className="logo" />
+                        <Menu
+                            theme="dark"
+                            mode="inline"
+                            openKeys={this.state.openKeys}
+                            onOpenChange={this.onOpenChange}
+                            style={{ width: 256 }}
+                        >
+                            <SubMenu
+                                key="sub1"
+                                title={
+                                    <span>
+              <Icon type="mail" />
+              <span>CORS</span>
+            </span>
+                                }
+                            >
+                                <Menu.Item key="1">CORS GET</Menu.Item>
+                            </SubMenu>
+                            <SubMenu
+                                key="sub2"
+                                title={
+                                    <span>
+              <Icon type="appstore" />
+              <span>TEST</span>
+            </span>
+                                }
+                            >
+                                <SubMenu key="sub3" title="Submenu">
+                                    <Menu.Item key="7">Option 7</Menu.Item>
+                                    <Menu.Item key="8">Option 8</Menu.Item>
+                                </SubMenu>
+                            </SubMenu>
+                        </Menu>
+                    </Sider>
+                    <Layout style={{ marginLeft: 200 }}>
+                        {/*<Header style={{ background: '#fff', padding: 0 }} />*/}
+                        <Content style={{ margin: '24px 16px 0', overflow: 'initial' }}>
+                            <div style={{ padding: 24, background: '#fff', textAlign: 'center' }}>
+                                <CorsRemote/>
+                            </div>
+                        </Content>
+                        <Footer style={{ textAlign: 'center' }}>Cyber Security Tools ©2020 Created by <a href="https://github.com/nic329" target="_blank" rel="noopener noreferrer">Dem0ns</a></Footer>
+                    </Layout>
+                </Layout>
+            </div>
+        );
+    }
 }
 
 export default App;
---
+++ src/CORS/remote.js
@@ -0,0 +1,65 @@
+import React from 'react';
+import {Select, Input, Button, message} from 'antd';
+const {Option} = Select;
+
+class Remote extends React.Component{
+
+    state = {
+      url: "http://127.0.0.1/",
+      method: "GET"
+    };
+    cors = () =>  {
+        var xhttp = new XMLHttpRequest();
+        xhttp.onreadystatechange = function() {
+            if(this.readyState === 4)
+                if (this.status === 200) {
+                    message.success("Perfect!");
+                    console.log((this.responseText));
+                    document.querySelector("#html").innerHTML = this.responseText
+                }else{
+                    message.error("CORS Fail.");
+                }
+        };
+        xhttp.open(this.state.method, this.state.url, true);
+        xhttp.withCredentials = true;
+        xhttp.send();
+    };
+    handleUrlChange = (event) => {
+        this.setState({
+            url : event.target.value,
+        })
+    };
+    handleMethodChange = (event) => {
+        this.setState({
+            method: event.target.value
+        });
+    };
+    selectBefore = (
+        <Select
+            defaultValue="GET"
+            style={{ width: 90 }}
+            value={this.state.method}
+            onChange={this.handleMethodChange}
+        >
+            <Option value="GET">GET</Option>
+            <Option value="POST">POST</Option>
+        </Select>
+    );
+
+    render() {
+        return (
+            <div>
+                <h2>CORS POC</h2>
+                <div style={{ marginBottom: 16 }}>
+                    <Input addonBefore={this.selectBefore} value={this.state.url} onChange={this.handleUrlChange}/>
+                </div>
+                <Button onClick={this.cors} type="primary">Fxxk It !</Button>
+                <hr/>
+                <div id="html">
+                </div>
+            </div>
+        )
+    }
+}
+
+export default Remote;
